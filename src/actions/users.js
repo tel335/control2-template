03b3b1a223084da781b7550fe6d248a2176986f8
ffierro <franco.fierro@ilogica.cl>
2022-04-20 21:18:58 -0400

    modificacion

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,20 +1,21 @@
 const userData = require('../data/users.js')
-const token = require('../utils/uuid.js')
+const Token = require('../utils/uuid.js')
 const config = require('../utils/constants.js')
-KEY = config.KEY
-SECRET = config.SECRET
+const KEY = config.KEY
+const SECRET = config.SECRET
 
 
 exports.getToken = (id,secret,key) => {
     let response =  []
-    
+    let aux = {}
     const usuarios = userData.getUsers()
     usuarios.forEach((element) => {
         if(element.id == id &&  KEY == key && SECRET == secret){
-            element.token = token.getUUIDV4()
-            response.push(element)
+            element.token = Token.getUUIDV4()
+            aux.token =   element.token
+            response.push(aux)
             
         }   
-    });
-    return response.token
+    })
+    return response
 }
